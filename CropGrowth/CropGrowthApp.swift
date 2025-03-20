bc8c7a0772420c4c6c5417855b4e684801954f2d
Xander Wanagel <xander.wanagel@gmail.com>
2025-03-20 02:49:04 +0100

    Initial Commit

---
+++ CropGrowth/CropGrowthApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropGrowthApp.swift
+//  CropGrowth
+//
+//  Created by Xander Wanagel on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CropGrowthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
